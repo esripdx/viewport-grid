7e1c918261afc80fa38d7c83222fb586ac281bb2
Nikolas Wise <nikolaswise@gmail.com>
2014-06-03 14:26:03 -0700

    stub css

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,43 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
+
+    'connect': {
+      'static': {
+        options: {
+          base: 'docs/build/',
+          hostname: '0.0.0.0',
+          port: 8001
+        }
+      }
+    },
+
+    'watch': {
+      sass: {
+        files: ['lib/scss/**/*.scss'],
+        tasks: ['sass'],
+        options: {
+          nospawn: true
+        }
+      }
+    },
+
+    'sass': {
+      dist: {
+        files: {
+          'dist/viewport-grid/viewport-grid.css': 'lib/scss/viewport-grid.scss'
+        }
+      }
+    }
   });
 
   require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask(
+    'default', [
+      'connect',
+      'sass',
+      'watch'
+    ]
+  );
 };
